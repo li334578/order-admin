9de0c5f3ea63b84899b5a1ae7b5ce179f65c0454
liwenbo <liwenbo4922@163.com>
2023-01-31 15:54:26 +0800

    init

---
+++ order_admin_java/src/main/java/com/example/order_admin_java/OrderAdminJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_admin_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAdminJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderAdminJavaApplication.class, args);
+    }
+
+}
